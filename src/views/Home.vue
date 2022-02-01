f27d26011594647b7cfc43e5417ef92f3c5ccafe
ClintZy101 <clielizek.joy@gmail.com>
2022-02-02 06:27:14 +0900

    getPost

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,97 +1,42 @@
 <template>
+<div v-if="showPosts">
 <div v-if="error">{{ error }}</div>
-<div v-if="posts.length">
-  <PostList  :posts="posts" />
+  <div v-if="posts.length">
+    <PostList :posts="posts" />
+  </div>
+  <div v-else>Loading...</div>
 </div>
-<div v-else>Loading...</div>
-<button @click="showPosts = !showPosts">Toggle Posts</button>
-<button @click="posts.pop()">Delete a Post</button>
   
-  <!-- <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-  <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-  <button @click="updateNinjaOne">Update NinjaOne</button>
-  <button @click="updateNinjaTwo">Update NinjaTwo</button>
+  <button @click="showPosts = !showPosts">Toggle Posts</button>
+  <button @click="posts.pop()">Delete a Post</button>
 
-  <div style="marginTop: 3rem">
-    <Computed />
-  </div>  -->
 
- 
 </template>
 
 <script>
-import { reactive, ref } from "@vue/reactivity";
-import Computed from '../components/Computed.vue';
-import PostList from '../components/PostList.vue'
+import { ref } from '@vue/reactivity';
+import Computed from "../components/Computed.vue";
+import PostList from "../components/PostList.vue";
+import getPosts from '../composables/getPosts'
 
 export default {
   name: "Home",
-  components:{
+  components: {
     PostList,
-    Computed
+    Computed,
   },
-  
+
   setup() {
-    //  const p = ref(null)
-    //  const name = ref('clint')
-    //  const age = ref(29)
-    //   const handleClick = () => {
-    //     // console.log(p, p.value)
-    //     // p.value.classList.add('test')
-    //     // p.value.textContent = 'hello'
-    //     // name = 'luigi'  {{this will not work}}
-    //     name.value = 'luigi'
-    //     age.value ='330'
-    //   }
-
-    const ninjaOne = ref({ name: "mario", age: 30 });
-    const ninjaTwo = reactive({ name: "luigi", age: 31 });
-
-    // You can't use primitive values (strings, boolean etc) in reactive
-    // 
-
-    const updateNinjaOne = () => {
-      ninjaOne.value.age += 1;
-    };
-
-    const updateNinjaTwo = () => {
-      ninjaTwo.age += 1;
-    };
-
-    const posts = ref([])
+    const {posts, error, load } = getPosts()
+    load()
 
     const showPosts = ref(true)
 
-    const error = ref(null)
-
-
-
-    const load = async() => {
-      try {
-        let data = await fetch('http://localhost:3000/posts')
-        if (!data.ok){
-          throw Error('No data available')
-        }
-        posts.value = await data.json()
-      }
-      catch (err) {
-        error.value = err.message
-        console.log(error.value)
-      }
-    }
-
-    load()
-
     return {
-      ninjaOne,
-      ninjaTwo,
+      error,
       posts,
-      updateNinjaOne,
-      updateNinjaTwo,
-      showPosts,
-      error
-    }
+      showPosts
+    };
   },
- 
 };
 </script>
